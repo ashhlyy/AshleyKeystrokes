48debb33f48f73f5e70285ad2c74ccd5a5781e99
ashly <86857592+ashhlyy@users.noreply.github.com>
2021-12-25 19:51:41 +0100

    Add files via upload

---
+++ net/ashley/util/GraphicUtil.java
@@ -0,0 +1,11 @@
+package net.ashley.util;
+
+import net.minecraft.client.gui.Gui;
+
+public class GraphicUtil {
+
+    public static void drawRectangle(int point1x, int point1y, int point2y, int color) {
+        Gui.drawRect(point1x, point1y, point2x, point2y, color);
+    }
+
+}
